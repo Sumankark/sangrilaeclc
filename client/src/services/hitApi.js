8514bfc33d28c542938722ce45b5619498d426c7
Sumankark <138770096+Sumankark@users.noreply.github.com>
2024-10-28 16:52:50 +0545

    Update hitApi.js

--- client/src/services/hitApi.js
+++ client/src/services/hitApi.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const hitApi = axios.create({
-  baseURL: "http://localhost:8080",
+    baseURL: "https://sangrilaeclc.onrender.com",
+  // baseURL: "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
