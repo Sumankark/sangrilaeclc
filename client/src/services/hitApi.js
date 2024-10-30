637d9f1e79ba08159793a7b6621b650d496d9130
sudip karki <sudipkarki@sudips-MacBook-Air.local>
2024-10-30 12:54:03 +0545

    9th commit

--- client/src/services/hitApi.js
+++ client/src/services/hitApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const hitApi = axios.create({
-    baseURL: "https://sangrilaeclc.onrender.com",
-  // baseURL: "http://localhost:8080",
+  // baseURL: "https://sangrilaeclc.onrender.com",
+  baseURL: "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
